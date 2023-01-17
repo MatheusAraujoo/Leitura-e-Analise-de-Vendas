a9e99fddc3079e59961dc20eccb6f9c013832966
Matheus Araujo Morais <matheus.araujo.morais@outlook.com>
2023-01-17 19:28:51 -0300

    O usuário pode agora selecionar uma opção e escolher se quer visualizar outra opção ou encerrar o programa.

--- exibir_dados.py
+++ exibir_dados.py
@@ -1,17 +1,51 @@
 from organizar_dados import OrganizarDados
-
+#Apresentação do programa
 print("Bem-vindo ao programa")
-print("Esse programa analisa um diretório contendo arquivos de vendas e devoluções.\n")
+print("Esse programa analisa um diretório contendo arquivos de vendas e devoluções de todas as unidades.\n")
 
-while True:
+flag = True
+while flag:
+    #Pergunta qual o diretório a ser analisado, caso o arquivo não seja encontrado, é perguntado novamente.
     EscolherDiretorio = input("Para começar, qual é o nome do diretório que deseja analisar?")
     try:
-        dados = OrganizarDados(EscolherDiretorio)
+        dados = OrganizarDados(EscolherDiretorio.strip().title())
     except FileNotFoundError:
         print("Arquivo não encontrado, tente novamente!")
         continue
     else:
-        dados.faturamento_produto()
+        #exibe as opções e pede para o usuário escolher uma
+        print("\n1 - Quantidade de vendas por produto\n"
+              "2 - Faturamento por produto\n"
+              "3 - Quantidade de vendas por loja\n"
+              "4 - Faturamento por loja\n")
+
+        opcao = input("Escolha uma das opções acima: ")
+
+        if opcao == '1':
+            dados.quantidade_vendas_produto()
+        if opcao == '2':
+            dados.faturamento_produto()
+        if opcao == '3':
+            dados.quantidade_vendas_loja()
+        if opcao == '4':
+            dados.faturamento_loja()
+
+        #Pergunta se deseja utilizar o programa novamente, caso o resultado seja diferente de 's' ou 'n',
+        #é perguntado novamente
+        while flag:
+            sn = input("Deseja fazer novamente? 's' ou 'n': ")
+            if sn == 's':
+                break
+            elif sn == 'n':
+                print("Muito obrigado por utilizar o programa!")
+                flag = False
+            else:
+                print("Entrada inválida. Digite 's' ou 'n'!")
+                continue
+
+
+
+
 
 
 
--- organizar_dados.py
+++ organizar_dados.py
@@ -21,19 +21,43 @@ class OrganizarDados():
         """Gera uma tabela e um gráfico contendo o total de vendas realizadas por produto """
         self.percorrer_arquivos()
         self.tabela_produtos = self.tabela_total.groupby('Produto').sum()
-        self.tabela_qtdvendas_produto = self.tabela_produtos[['Quantidade Vendida']].sort_values(by='Quantidade Vendida', ascending=False)
+        self.tabela_qtdvendas_produto = self.tabela_produtos[['Quantidade Vendida']]\
+            .sort_values(by='Quantidade Vendida', ascending=False)
         print(self.tabela_qtdvendas_produto)
-        self.grafico = px.bar(self.tabela_qtdvendas_produto, x=self.tabela_qtdvendas_produto.index, y='Quantidade Vendida')
+        self.grafico = px.bar(self.tabela_qtdvendas_produto, x=self.tabela_qtdvendas_produto.index,
+                              y='Quantidade Vendida')
         self.grafico.show()
 
     def faturamento_produto(self):
         """Gera uma tabela e um gráfico contendo o total de faturamento por produto """
         self.percorrer_arquivos()
         self.tabela_total['Faturamento'] = self.tabela_total['Quantidade Vendida'] * self.tabela_total['Preco Unitario']
-        self.tabela_faturamento = self.tabela_total.groupby('Produto').sum()
-        self.tabela_faturamento_produto = self.tabela_faturamento[['Faturamento']].sort_values(by='Faturamento', ascending=False)
+        self.tabela_produtos = self.tabela_total.groupby('Produto').sum()
+        self.tabela_faturamento_produto = self.tabela_produtos[['Faturamento']]\
+            .sort_values(by='Faturamento', ascending=False)
         print(self.tabela_faturamento_produto)
         self.grafico = px.bar(self.tabela_faturamento_produto, x=self.tabela_faturamento_produto.index, y='Faturamento')
+        self.grafico.show()
+
+    def quantidade_vendas_loja(self):
+        """Gera uma tabela e um gráfico contendo o total de vendas realizadas por loja """
+        self.percorrer_arquivos()
+        self.tabela_loja = self.tabela_total.groupby('Loja').sum()
+        self.tabela_qtdvendas_loja = self.tabela_loja[['Quantidade Vendida']]\
+            .sort_values(by='Quantidade Vendida', ascending=False)
+        print(self.tabela_qtdvendas_loja)
+        self.grafico = px.bar(self.tabela_qtdvendas_loja, x=self.tabela_qtdvendas_loja.index, y='Quantidade Vendida')
+        self.grafico.show()
+
+    def faturamento_loja(self):
+        """Gera uma tabela e um gráfico contendo o total de faturamento por produto """
+        self.percorrer_arquivos()
+        self.tabela_total['Faturamento'] = self.tabela_total['Quantidade Vendida'] * self.tabela_total['Preco Unitario']
+        self.tabela_loja = self.tabela_total.groupby('Loja').sum()
+        self.tabela_faturamento_loja = self.tabela_loja[['Faturamento']] \
+            .sort_values(by='Faturamento', ascending=False)
+        print(self.tabela_faturamento_loja)
+        self.grafico = px.bar(self.tabela_faturamento_loja, x=self.tabela_faturamento_loja.index, y='Faturamento')
         self.grafico.show()
 
 
